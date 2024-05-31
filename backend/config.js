47802e7bdc130233ad0f50bacff289b049c8a089
Jeee-em <je.empantaleon@gmail.com>
2024-05-31 22:32:49 +0800

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://jmeow:jmeow@mern-blog.ykjftm1.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
